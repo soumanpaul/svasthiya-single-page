acd85acd6188c28cc251036d5d5b3bc5ee476c38
ssouman <souman1729@gmail.com>
2020-03-30 20:10:59 +0530

    sjdfk

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,59 +1,101 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
-    Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Navbar, NavbarBrand, Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    // CarouselCaption
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+const items = [
+    {
+        src: 'assets/images/mri1.jpg',
+        altText: 'Slide 1',
+        caption: 'Slide 1',
+        header: 'Slide 1 Header',
+        key: '1'
+    },
+    {
+        src: 'assets/images/mri2.jpg',
+        altText: 'Slide 2',
+        caption: 'Slide 2',
+        header: 'Slide 2 Header',
+        key: '2'
+    },
+    {
+        src: 'assets/images/mri3.jpg',
+        altText: 'Slide 3',
+        caption: 'Slide 3',
+        header: 'Slide 3 Header',
+        key: '3'
+    }
+];
+const Header = () => {
 
-class Header extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isNavOpen: false,
-            isModalOpen: false
-        };
-        this.toggleNav = this.toggleNav.bind(this);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
     }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
     }
 
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
 
-    render() {
+    const slides = items.map((item) => {
         return (
-            <React.Fragment>
-                <Navbar dark expand="md">
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="nav-logo" href="/">
-                            <img src="assets/images/icon1.png" height="70" width="80"
-                                alt="Ristorante Con Fusion" />
-                        </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/">
-                                        <div className="Title">Svasthiya</div>
-
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
-                </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header justify-content-center">
-                       
-                        </div>
-                    </div>
-                </Jumbotron>
-            </React.Fragment>
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
+            >
+                <img src={item.src} alt={item.altText} height="500" width="100%" />
+                {/* <CarouselCaption captionText={item.caption} captionHeader={item.caption} /> */}
+            </CarouselItem>
         );
-    }
+    });
+
+
+
+
+
+
+    return (
+        <React.Fragment>
+            <Navbar dark expand="md">
+                <div className="container">
+
+                    <NavbarBrand className="nav-logo" href="/">
+                        <img src="assets/images/icon1.png" height="70" width="80"
+                            alt="Ristorante Con Fusion" />
+                    </NavbarBrand>
+                    <NavLink className="nav-link" to="/">
+                        <div className="Title">Svasthiya</div>
+                    </NavLink>
+                </div>
+            </Navbar>
+            <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+            </Carousel>
+        </React.Fragment>
+    );
+
 }
 
 export default Header;
